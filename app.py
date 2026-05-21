5a3a0ab96679665535a4a33ee75d3c16657f946c
manso123498-sys <manso123498@gmail.com>
2026-05-21 15:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,10 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
        'Sharjah', 'Mohali', 'Bengaluru']
 
-pipe = pickle.load(open('pipe.pkl','rb'))
+import streamlit as st
+import joblib
+
+pipe = joblib.load('pipe.pkl')
 st.title('IPL Win Predictor')
 
 col1, col2 = st.beta_columns(2)
